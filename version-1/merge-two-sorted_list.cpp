33cf5ce10630ede619d729f30318ee0735623e49
shitou <yangshuo.1125@163.com>
2016-03-21 21:43:21 +0800

    merge-two-list的改进版本，可以在leetcode上AC

--- version-1/merge-two-sorted_list.cpp
+++ version-1/merge-two-sorted_list.cpp
@@ -11,54 +11,73 @@
  */
  //（默认链表是从小到大的顺序排列）
 
- //当前版本的算法，可以实现功能，但运行时间不符合要求，待改进
+ //当前版本的算法，经过了改进，已在leetcode上AC
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
    
-   struct ListNode *p,*q,*temp;
+   struct ListNode *p,*q,*temp,*temp2;
    //先判断两个list中是否有某一个为空链表
    if(l1==NULL||l2==NULL){
       return (l1==NULL)?l2:l1;
    }
    else{
-      //先比较两个链表首元素大小，确定以哪个为基础生成新聊表
-   if((l1->val)>=(l2->val)){
-        p=l1;
-        q=l2;
-        while(p!=NULL&&q->next!=NULL){
-        if(p->val>=q->next->val) {
-            q=q->next;
-        }  
-        else{
+    //判断一下l1和l2谁的首元素大，然后相应地确定以哪个list为基准
+    if(l1->val<=l2->val)//l1为基准
+    {
+       p=l1;
+       q=l2;      
+       while(q!=NULL){
+          temp2=q->next;//要先保存下一结点信息
+         if(q->val>=p->val){
+           while(p!=NULL&&q->val>=p->val){ 
             temp=p;
-            temp->next=q->next;
-            q->next=temp;
-            p=p->next;
-            }
-        }
-        if(p!=NULL){
-           q->next=p;
+            p=p->next; 
           }
-        return l2;
-   }
-   else{
-        p=l2;
-        q=l1;
-        while(p!=NULL&&q->next!=NULL){
-        if(p->val>=q->next->val) {
-            q=q->next;
-        }  
-        else{
+          q->next=temp->next;
+          temp->next=q;
+          temp=temp->next;//因为新插入了元素，所以temp位置发生了变化
+         }
+         else{
+            q->next=temp->next;
+            temp->next=q;
+            temp=temp->next;//因为新插入了元素，所以temp位置发生了变化
+         }
+         if(p==NULL)//如果出现l1长度小于l2长度时
+         {
+           q->next=temp2;
+           break;
+         }
+         q=temp2;
+         }
+       return l1; 
+    }
+    else//l2为基准
+    {
+      p=l2;
+      q=l1;
+      while(q!=NULL){
+          temp2=q->next;//要先保存下一结点信息
+         if(q->val>=p->val){
+           while(p!=NULL&&q->val>=p->val){ 
             temp=p;
-            temp->next=q->next;
-            q->next=temp;
-            p=p->next;
-            }
-        }
-        if(p!=NULL){
-           q->next=p;
+            p=p->next; 
           }
-   	    return l1;
-   }
-
+          q->next=temp->next;
+          temp->next=q;
+          temp=temp->next;
+         }
+         else{
+            q->next=temp->next;
+            temp->next=q;
+            temp=temp->next;//因为新插入了元素，所以temp位置发生了变化（只要插入元素就会后移）
+         }
+         if(p==NULL)
+         {
+           q->next=temp2;
+           break;
+         }
+         q=temp2;
+       }  
+       return l2; 
+    }
    }
 }
